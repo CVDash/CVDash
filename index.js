a9ca4cd865dfb52362ca042bf3663e137633ec66
RCVEliteGaming <rcv@elitegaming.dk>
2024-10-18 13:39:08 +0200

    update

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ function playaudiogrenade() {
   },100);
 }
 
-function playaudiorocket() {
-  var audio = document.getElementById("rocketlauncher");
+function playaudiocannon() {
+  var audio = document.getElementById("cannon");
   audio.currentTime=0;
   audio.load();
   audio.play();
@@ -26,8 +26,63 @@ function playaudiorocket() {
   },100);
 }
 
+function playaudiorocketlauncher() {
+  var audio = document.getElementById("rocketlauncher");
+  var minigun = document.getElementById("minigun");
+  var switchweapon = document.getElementById("painkiller");
+
+   if (switchweapon.classList.contains("minigun")) {
+    minigun.currentTime=0;
+    minigun.load();
+    minigun.play();
+    switchweapon.classList.remove("minigun")
+
+   } else {
+    minigun.pause();
+    audio.currentTime=0;
+    audio.load();
+    audio.play();
+    switchweapon.classList.add("minigun")
+   }
+
+
+   
+
+  
+
+  var backgroundfirerocket = document.getElementById ("painkiller");
+  backgroundfirerocket.classList.add ('fire');
+
+  setTimeout(function(){
+    backgroundfirerocket.classList.remove ('fire'); 
+  },100);
+}
+
+
+
+function playaudioshotgun() {
+  var audio = document.getElementById("shotgun");
+  audio.currentTime=0;
+  audio.load();
+  audio.play();
+
+  var backgroundfirerocket = document.getElementById ("quake2");
+  backgroundfirerocket.classList.add ('fire');
+
+  setTimeout(function(){
+    backgroundfirerocket.classList.remove ('fire'); 
+  },100);
+}
+
+
 
 var volumeG = document.getElementById ("grenadelauncher")
 volumeG.volume = 0.1;
+var volumeG = document.getElementById ("shotgun")
+volumeG.volume = 0.1;
 var volumeR = document.getElementById ("rocketlauncher")
+volumeR.volume = 0.1;
+var volumeR = document.getElementById ("minigun")
+volumeR.volume = 0.1;
+var volumeR = document.getElementById ("cannon")
 volumeR.volume = 0.1;
